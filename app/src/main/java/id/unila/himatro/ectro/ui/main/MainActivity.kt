f4b96ebcc4d5bd4e9b2282ed998e65a7eb0b5667
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-07-31 01:10:33 +0700

    add bottom nav

--- app/src/main/java/id/unila/himatro/ectro/ui/main/MainActivity.kt
+++ app/src/main/java/id/unila/himatro/ectro/ui/main/MainActivity.kt
@@ -4,7 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.bumptech.glide.Glide
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import id.unila.himatro.ectro.R
 import id.unila.himatro.ectro.databinding.ActivityMainBinding
 import id.unila.himatro.ectro.ui.auth.LoginActivity
@@ -18,30 +24,53 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_home)
+
         binding.apply {
-            Glide.with(this@MainActivity)
-                .load(R.drawable.ic_default_profile)
-                .into(ivUserPhoto)
+            val bottomNavView: BottomNavigationView = bottomNavView
+            bottomNavView.background = null
+            bottomNavView.menu.getItem(1).isEnabled = false
 
-            Glide.with(this@MainActivity)
-                .load(R.drawable.sample_announcement_image)
-                .into(announcement)
+            val appBarConf = AppBarConfiguration(
+                setOf(
+                    R.id.nav_home,
+                    R.id.nav_profile
+                )
+            )
 
-            if (tvUserName.text.isNotEmpty()) {
-                tvGreeting.visibility = View.VISIBLE
-                tvNotLoginYet.visibility = View.GONE
-            } else {
-                tvNotLoginYet.visibility = View.VISIBLE
-            }
+            setupActionBarWithNavController(navController)
+            bottomNavView.setupWithNavController(navController)
 
-            ivUserPhoto.setOnClickListener {
-
-            }
-
-            tvNotLoginYet.setOnClickListener {
-                startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+            fabScan.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Scan Button", Toast.LENGTH_SHORT).show()
             }
         }
+
+    //        binding.apply {
+//            Glide.with(this@MainActivity)
+//                .load(R.drawable.ic_default_profile)
+//                .into(ivUserPhoto)
+//
+//            Glide.with(this@MainActivity)
+//                .load(R.drawable.sample_announcement_image)
+//                .into(announcement)
+//
+//            if (tvUserName.text.isNotEmpty()) {
+//                tvGreeting.visibility = View.VISIBLE
+//                tvNotLoginYet.visibility = View.GONE
+//            } else {
+//                tvNotLoginYet.visibility = View.VISIBLE
+//            }
+//
+//            ivUserPhoto.setOnClickListener {
+//
+//            }
+//
+//            tvNotLoginYet.setOnClickListener {
+//                startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+//            }
+//        }
 
     }
 }
